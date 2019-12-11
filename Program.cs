a3e335572b3f8958f285a4f5e0b6ef05ed400893
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-11 11:14:39 -0500

    Initial commit of take-home-pay project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace take_home_pay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
