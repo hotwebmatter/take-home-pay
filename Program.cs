dc3202a1369eca4f0fc6dab539246717c80512b9
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-11 11:16:39 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace take_home_pay
 {
@@ -6,7 +7,7 @@ namespace take_home_pay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
